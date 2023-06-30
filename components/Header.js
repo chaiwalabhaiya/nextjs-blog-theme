8f0c107fbc288bfd815e1ff5f025b9261ca85a8b
chaiwalabhaiya <138197006+chaiwalabhaiya@users.noreply.github.com>
2023-06-30 12:53:34 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ export default function Header({ name }) {
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
-          <a>{name}bst</a>
+          <a href="https://financeappsfaq.com/does-wawa-accept-apple-pay/">Does Wawa accept Apple Pay?</a>
         </Link>
       </p>
     <p>vbbvbv</p>
